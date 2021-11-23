1ecea053b43653526493578b5bcbb9d531f5f527
GitHub Action <action@github.com>
2021-11-23 02:47:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pilockhulmes/pluginexporlor/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pilockhulmes.pluginexporlor.services
+
+import com.github.pilockhulmes.pluginexporlor.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/test/kotlin/com/github/pilockhulmes/pluginexporlor/MyPluginTest.kt
+++ src/test/kotlin/com/github/pilockhulmes/pluginexporlor/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pilockhulmes.pluginexporlor
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
